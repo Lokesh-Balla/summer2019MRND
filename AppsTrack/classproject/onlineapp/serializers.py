a4d08964fbce45e1c9b44fd5e78a0a63f029be9d
Lokesh-Balla <lokesh.balla123@gmail.com>
2019-06-12 22:51:33 +0530

    added api calls for students

--- AppsTrack/classproject/onlineapp/serializers.py
+++ AppsTrack/classproject/onlineapp/serializers.py
@@ -7,3 +7,48 @@ class CollegeSerializer(serializers.ModelSerializer):
     class Meta:
         model = College
         fields = ('id', 'name', 'location', 'acronym', 'contact')
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('id', 'name', 'dob', 'email', 'db_folder', 'dropped_out', 'college')
+
+
+class MockTest1Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = MockTest1
+        fields = ('problem1', 'problem2', 'problem3', 'problem4', 'total')
+
+
+class StudentDetailsSerializer(serializers.ModelSerializer):
+    mocktest1 = MockTest1Serializer()
+
+    class Meta:
+        model = Student
+        fields = ('name', 'dob', 'email', 'db_folder', 'dropped_out', 'college', 'mocktest1')
+
+    def create(self, validated_data):
+        marks_data = validated_data.pop('mocktest1')
+        student = Student.objects.create(**validated_data)
+        MockTest1.objects.create(student=student, **marks_data)
+        return student
+
+    def update(self, instance, validated_data):
+        student = validated_data.pop('mocktest1')
+        mocktest1 = instance.mocktest1
+        instance.name = validated_data.get('name', instance.name)
+        instance.dob = validated_data.get('dob', instance.dob)
+        instance.email = validated_data.get('email', instance.email)
+        instance.db_folder = validated_data.get('db_folder', instance.db_folder)
+        instance.dropped_out = validated_data.get('dropped_out', instance.dropped_out)
+        instance.college = validated_data.get('college', instance.college)
+        instance.save()
+
+        mocktest1.problem1 = student.get('problem1', mocktest1.problem1)
+        mocktest1.problem2 = student.get('problem2', mocktest1.problem2)
+        mocktest1.problem3 = student.get('problem3', mocktest1.problem3)
+        mocktest1.problem4 = student.get('problem4', mocktest1.problem4)
+        mocktest1.total = student.get('total', mocktest1.total)
+        mocktest1.save()
+        return instance
--- AppsTrack/classproject/onlineapp/views/college_api.py
+++ AppsTrack/classproject/onlineapp/views/college_api.py
@@ -1,12 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from onlineapp.serializers import *
 
 
-@api_view(['GET', 'POST'])
-def GetColleges(request, **kwargs):
-    if request.method == 'GET':
+class GetColleges(APIView):
+    def get(self, request, **kwargs):
         if kwargs:
             colleges = College.objects.all().filter(**kwargs)
         else:
@@ -16,10 +15,63 @@ def GetColleges(request, **kwargs):
             return Response({'colleges': serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
-    elif request.method == 'POST':
+
+    def post(self, request, **kwargs):
         college = CollegeSerializer(data=request.data)
         if college.is_valid():
             college.save()
             return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, **kwargs):
+        if kwargs:
+            college = CollegeSerializer(data=request.data, instance=College.objects.get(**kwargs))
+            if college.is_valid():
+                college.save()
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, **kwargs):
+        if kwargs:
+            College.objects.get(**kwargs).delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetStudents(APIView):
+    def get(self, request, **kwargs):
+        if kwargs.get("spk"):
+            students = Student.objects.filter(pk=kwargs['spk'])
+        else:
+            students = Student.objects.filter(college=kwargs['pk'])
+        if students:
+            serializer = StudentDetailsSerializer(students, many=True)
+            return Response({'students': serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request, **kwargs):
+        student = StudentDetailsSerializer(data=request.data)
+        if student.is_valid():
+            student.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, **kwargs):
+        student = StudentDetailsSerializer(data=request.data, instance=Student.objects.get(id=kwargs['spk']))
+        if student.is_valid():
+            student.save()
+            return Response(student.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, **kwargs):
+        if kwargs:
+            Student.objects.get(**kwargs).delete()
+            return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
